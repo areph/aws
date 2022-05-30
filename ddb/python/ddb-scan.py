a14238558a011c3ec35cd5afae07e699f6b37cb2
Shinji Miura <miushinj@amazon.co.jp>
2022-05-30 16:26:46 +0900

    Add scan pattern

--- ddb/python/ddb-scan.py
+++ ddb/python/ddb-scan.py
@@ -1,7 +1,9 @@
 import boto3
+from boto3.dynamodb.conditions import Key
 
 
 def scan():
+    print('---ex) Music Table Scan---')
     dynamodb = boto3.resource('dynamodb',
                               endpoint_url='http://localhost:8000',
                               region_name='ap-northeast-1',
@@ -12,6 +14,18 @@ def scan():
     res = table.scan()
     for item in res['Items']:
         print(item)
+
+    print('---ex) MusicID conditions---')
+    res = table.query(KeyConditionExpression=Key('MusicID').eq('2'))
+
+    items = res['Items']
+    print(items)
+
+    print('---ex) MusicID conditions with SortKey---')
+    res = table.query(KeyConditionExpression=Key(
+        'MusicID').eq('1') & Key('ReleaseDate').eq('20220608'))
+    items = res['Items']
+    print(items)
 
 
 if __name__ == '__main__':
