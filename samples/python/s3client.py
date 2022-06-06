c61d37efcab7b3c0079470c01d93c304855d04a6
Shinji Miura <miushinj@amazon.co.jp>
2022-06-06 19:11:00 +0900

    Modify mod05 s3 sdk sample code

--- samples/python/s3client.py
+++ samples/python/s3client.py
@@ -1,4 +1,6 @@
+# 依存関係を定義
 import boto3
+import uuid
 
 # セッションとして認証情報を取得
 session = boto3.session.Session()
@@ -9,9 +11,15 @@ current_region = session.region_name
 # セッションから低レベルAPIを操作
 client = session.client('s3')
 
+# セッションからではなくboto3からclientを生成することも可能
+#client = boto3.client('s3', region_name=current_region)
+
+bucket_name = f'dev-bucket-{uuid.uuid4()}'
+print(f'作成するバケット名: {bucket_name}')
+
 # バケットを作成する方法に注目
 client.create_bucket(
-    Bucket='${作成したいバケット名}',
+    Bucket=bucket_name,
     CreateBucketConfiguration={
         'LocationConstraint': current_region
     }
--- samples/python/s3resource.py
+++ samples/python/s3resource.py
@@ -1,4 +1,6 @@
+# 依存関係を定義
 import boto3
+import uuid
 
 # セッションとして認証情報を取得
 session = boto3.session.Session()
@@ -10,8 +12,14 @@ print(current_region)
 # セッションから高レベルAPIを操作
 resource = session.resource('s3')
 
+# セッションからではなくboto3からclientを生成することも可能
+#resource = boto3.resource('s3', region_name=current_region)
+
+bucket_name = f'dev-bucket-{uuid.uuid4()}'
+print(f'作成するバケット名: {bucket_name}')
+
 # バケットを作成する方法に注目
-bucket = resource.Bucket('${作成したいバケット名}')
+bucket = resource.Bucket(bucket_name)
 bucket.create(
     CreateBucketConfiguration={
         'LocationConstraint': current_region
